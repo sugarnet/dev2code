e89b7a09c07046c2fc511aef05bd2b7d1d985ef6
Diego Scifo <dscifo@aconcaguasf.com>
2019-06-19 13:49:59 -0300

    desde el trabajo

---
+++ spring_demo_one/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Practica el saque por veinte minutos!!!";
+	}
+
+}
