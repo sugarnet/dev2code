a747313ba47f7ece37a0a6f7787780080838eb57
Diego Scifo <dscifo@aconcaguasf.com>
2019-07-12 14:06:38 -0300

    desde el trabajo

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,26 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Practice in the brick dust field";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return this.fortuneService.getFortune();
 	}
 
 }
