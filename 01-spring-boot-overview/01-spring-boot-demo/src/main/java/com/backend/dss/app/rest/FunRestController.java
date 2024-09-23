07aeb9d6f8ae6d26159a941221a9e17bcf110ae2
dscifo <diego.scifo@globallogic.com>
2024-09-23 11:00:13 -0300

    Spring Boot Overview

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/backend/dss/app/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.backend.dss.app.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
---
+++ 01-spring-boot-overview/03-actuator-demo/src/main/java/com/backend/dss/app/rest/FunRestController.java
@@ -0,0 +1,23 @@
+package com.backend.dss.app.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
+    }
+}
