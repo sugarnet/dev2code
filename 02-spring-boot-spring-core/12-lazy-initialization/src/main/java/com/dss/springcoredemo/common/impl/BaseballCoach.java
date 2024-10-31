1aeae5b7b6bfe6a17a51d901a450c3ec0a1e1aa1
dscifo <diego.scifo@globallogic.com>
2024-10-31 18:00:26 -0300

    add new project lazy initialization

---
+++ 02-spring-boot-spring-core/12-lazy-initialization/src/main/java/com/dss/springcoredemo/common/impl/BaseballCoach.java
@@ -0,0 +1,17 @@
+package com.dss.springcoredemo.common.impl;
+
+import com.dss.springcoredemo.common.Coach;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spending 30 minutes in batting practice";
+    }
+}
