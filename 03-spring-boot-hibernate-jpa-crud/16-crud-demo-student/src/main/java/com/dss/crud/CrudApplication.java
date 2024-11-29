5f7a69f2028e7f5070b64a7f72c67ecd79fabec4
Diego Scifo <diego.scifo@globallogic.com>
2024-11-29 18:07:04 -0300

    new crud jpa project

---
+++ 03-spring-boot-hibernate-jpa-crud/16-crud-demo-student/src/main/java/com/dss/crud/CrudApplication.java
@@ -0,0 +1,20 @@
+package com.dss.crud;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return runner -> System.out.println("Welcome to Spring Boot Application");
+	}
+
+}
